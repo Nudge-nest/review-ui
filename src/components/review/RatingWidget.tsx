fee5d940ed8639994a744100383579c80eda5b5d
Olujuwon Alabi <ayoalabi0@gmail.com>
2025-08-01 08:34:07 +0300

    Fix: Refcator how rating widget handles products with and without images

--- src/components/review/RatingWidget.tsx
+++ src/components/review/RatingWidget.tsx
@@ -12,6 +12,22 @@ interface RatingWidgetProps {
 const MAX_RATING = 5;
 const RATING_ARRAY = Array.from({ length: MAX_RATING });
 
+const RatingWithProduct: FC<{ itemName: string, image? : string }> = ({ itemName, image }) => {
+    return (
+        <>
+            <img
+                src={image || 'https://placehold.co/300x300'}
+                className="h-15 w-15 rounded mb-4 block mx-auto"
+                alt={`${itemName}-image`}
+            />
+            <p className={`text-base text-balance`}>
+                {`Your rating for `}
+                <b>{` ${itemName || ''}`}</b>?
+            </p>
+        </>
+    );
+};
+
 const RatingWidget: FC<RatingWidgetProps> = ({ product, result, isCompleted }) => {
     const [selectedRating, setSelectedRating] = useState<number>(0);
     const { name, image, id } = product;
@@ -38,15 +54,13 @@ const RatingWidget: FC<RatingWidgetProps> = ({ product, result, isCompleted }) =
     return (
         <div className={`pt-12`}>
             <div className={`w-full text-center`}>
-                <img
-                    src={image || 'https://placehold.co/300x300'}
-                    className="h-15 w-15 rounded mb-4 block mx-auto"
-                    alt={`${name}-image`}
-                />
-                <p className={`text-base text-balance`}>
-                    {`Your rating for `}
-                    <b>{` ${name || ''}`}</b>?
-                </p>
+                {image ? (
+                    <RatingWithProduct itemName={name} image={image} />
+                ) : (
+                    <p className={`text-base text-balance`}>
+                        <b>{`${name || ''}`}</b>
+                    </p>
+                )}
             </div>
             <div className="w-full p-2 flex justify-center gap-4">
                 {RATING_ARRAY.map((_, idx) => {
