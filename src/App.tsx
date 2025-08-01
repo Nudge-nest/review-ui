9f30e33e5b15bf4b53d8f28d40df04bddea7e253
Olujuwon Alabi <ayoalabi0@gmail.com>
2025-08-01 16:20:17 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import NotFound from './components/NotFound.tsx';
 import ReviewPage from './pages/ReviewPage.tsx';
 import StoreReviewPage from './pages/StoreReviewPage.tsx';
 import ReviewConfigsPage from './pages/ReviewConfigsPage.tsx';
-import { ConfigsLayout, Layout } from './components/Layout.tsx';
+import { ConfigsLayout, Layout, ReviewsListLayout } from './components/Layout.tsx';
+import ReviewsListPage from './pages/ReviewsListPage.tsx';
+
+//ShopId = MTY3NTgwMjk3MzU0
+//merchantId = 68414ac959456a2575dd1aae
 
 const App = () => {
     return (
@@ -17,6 +21,9 @@ const App = () => {
             </Route>
             <Route element={<ConfigsLayout />}>
                 <Route path="configs/:merchantId" element={<ReviewConfigsPage />} />
+            </Route>
+            <Route element={<ReviewsListLayout />}>
+                <Route path="reviews/:shopId" element={<ReviewsListPage />} />
             </Route>
             <Route path="*" element={<NotFound />} />
         </Routes>
